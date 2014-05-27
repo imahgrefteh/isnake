16d6269b99ac645adb08a25d3c1ecf3d17c928e6
imahgrefteh <imahrefteh@yahoo.com>
2014-05-26 21:45:10 -0700

    adding source

---
+++ src/com/badlogic/androidgames/framework/Configuration2.java
@@ -0,0 +1,11 @@
+package com.badlogic.androidgames.framework;
+
+import com.badlogic.androidgames.framework.impl.AndroidGame;
+
+import android.app.Activity;
+
+public class Configuration2  extends Activity{
+	
+	  public static AndroidGame context = null;
+
+}
